907ea60ea5e3b14ad02b95e5e699737398d966da
Pablo Buendia <buenaspablo@gmail.com>
2019-01-27 01:27:08 -0300

    Commit 1

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping("/")
-	public String showPge() {
+	public String showPage() {
 		return "main-menu";
 	}
 	
