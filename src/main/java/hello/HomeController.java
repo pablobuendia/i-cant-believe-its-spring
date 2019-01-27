154c2a713e0b923b68fcb54243ff6fe54fe9ed97
Pablo Buendia <buenaspablo@gmail.com>
2019-01-27 01:25:27 -0300

    Initial commit

---
+++ src/main/java/hello/HomeController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPge() {
+		return "main-menu";
+	}
+	
+	
+}
