11e3ff52e6b19702524a6daacf2371e065711b23
pablobuendia <13057992+pablobuendia@users.noreply.github.com>
2021-03-27 19:02:30 -0300

    User entity

--- src/main/java/com/pablo/domain/User.java
+++ src/main/java/com/pablo/domain/User.java
@@ -1,14 +1,65 @@
 package com.pablo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
-  private String name;
 
-  public String getName() {
-    return name;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String email;
+  private String password;
+  @Column(name = "first_name")
+  private String firstname;
+  @Column(name = "last_name")
+  private String lastname;
+
+  public int getId() {
+    return id;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setId(int id) {
+    this.id = id;
   }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
 
 }
