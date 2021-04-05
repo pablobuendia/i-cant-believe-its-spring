9793a1d990e9ff863472da252e487de0a2a3e7bb
pablobuendia <13057992+pablobuendia@users.noreply.github.com>
2021-04-04 23:58:23 -0300

    Update Mappings

--- src/main/java/com/pablo/controllers/DoctorSearchController.java
+++ src/main/java/com/pablo/controllers/DoctorSearchController.java
@@ -2,8 +2,7 @@ package com.pablo.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.pablo.domain.Doctor;
@@ -23,7 +22,7 @@ public class DoctorSearchController {
    * @param specialty
    * @return
    */
-  @RequestMapping(value = "/doctors", method = RequestMethod.GET, produces = "application/json")
+  @GetMapping(value = "/doctors", produces = "application/json")
   public DoctorList searchDoctor(
       @RequestParam(value = "location", required = false) String location,
       @RequestParam(value = "specialty", required = false) String specialty) {
--- src/main/java/com/pablo/controllers/LoginController.java
+++ src/main/java/com/pablo/controllers/LoginController.java
@@ -1,15 +1,15 @@
 package com.pablo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/account/login")
 public class LoginController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String login() {
-		return "login";
-	}
+  @GetMapping()
+  public String login() {
+    return "login";
+  }
 }
