381fce04c7cbd32df7e638f1da304bac59d12d97
pablobuendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-01 14:34:53 -0300

    He made lemonade

---
+++ src/main/java/com/pablo/controllers/AdministrativeRepository.java
@@ -0,0 +1,10 @@
+package com.pablo.controllers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.pablo.domain.Administrative;
+
+@Repository
+public interface AdministrativeRepository extends JpaRepository<Administrative, Integer> {
+
+}
