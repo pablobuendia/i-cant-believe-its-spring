cd5b93db72ef7f90c1364eadba436cf31395e608
pablobuendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-01 14:39:13 -0300

    Dumb down the SpringAppApplication.java

--- src/main/java/com/pablo/SpringAppApplication.java
+++ src/main/java/com/pablo/SpringAppApplication.java
@@ -2,18 +2,14 @@ package com.pablo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @SpringBootApplication // <-- Look it says Spring Boot, that's what I'm using! Isn't it nice?
-public class SpringAppApplication extends SpringBootServletInitializer {
+@EnableJpaAuditing
+public class SpringAppApplication {
 
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(SpringAppApplication.class);
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(SpringAppApplication.class, args);
