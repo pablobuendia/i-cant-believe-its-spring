0b8e6b6391fdd9cf16fc04cc9cb910eba335eb4a
pablobuendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-05 23:17:44 -0300

    Messing with the AppConfig

--- src/main/java/com/pablo/configuration/AppConfig.java
+++ src/main/java/com/pablo/configuration/AppConfig.java
@@ -1,22 +1,18 @@
 package com.pablo.configuration;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration // To indicate the class can have one or more @Bean annotated methods. Is
-				// meta-annotated with @Component
-@EntityScan("com.pablo.domain") // Used to configure the base packages, which are used to
-								// scan the packages for JPA entity classes.
-@EnableTransactionManagement // This is used to register one or more Spring components,
-								// which are used to power annotation-driven transaction
-								// management capabilites.
-@PropertySource("classpath:application.properties") // These are used in conjuction with a
-													// @Configuration method.
-@EnableAutoConfiguration
-public class AppConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class AppConfig extends WebMvcConfigurationSupport {
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
 }
