b2c3229f851921b002699cf9261d389d435bcc07
pablobuendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-20 23:31:37 -0300

    Changes to the dependencies and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/js/App.js',
+    devtool: 'eval-source-map',
+    cache: true,
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'src/main/resources/templates/js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|eot|otf|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    }
+};
