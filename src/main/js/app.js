8ca099a04f1e1590ab18abdb52590ba45dc2b619
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-24 16:13:45 -0300

    Add new event handlers for App

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -15,7 +15,12 @@ class App extends React.Component { // <1>
 
 	constructor(props) {
 		super(props);
-		this.state = {administratives: []};
+		this.state = {administratives: [], attributes: [], pageSize: 2, links: {}};
+		this.updatePageSize = this.updatePageSize.bind(this);
+		this.onCreate = this.onCreate.bind(this);
+		this.onUpdate = this.onUpdate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
+		this.onNavigate = this.onNavigate.bind(this);
 	}
 
 	componentDidMount() { // <2>
@@ -24,11 +29,107 @@ class App extends React.Component { // <1>
 		});
 	}
 
-	render() { // <3>
+	// tag::create[]
+	onCreate(newAdministrative) {
+		const self = this;
+		follow(client, root, ['administratives']).then(administrativeCollection => {
+			return client({
+				method: 'POST',
+				path: administrativeCollection.entity._links.self.href,
+				entity: newAdministrative,
+				headers: {'Content-Type': 'application/json'}
+			})
+		}).then(response => {
+			return follow(client, root, [
+				{rel: 'administratives', params: {'size': this.state.pageSize}}]);
+		}).done(response => {
+			if (typeof response.entity._links.last !== "undefined") {
+				this.onNavigate(response.entity._links.last.href);
+			} else {
+				this.onNavigate(response.entity._links.self.href);
+			}
+		});
+	}
+	// end::create[]
+
+	// tag::update[]
+	onUpdate(administrative, updatedAdministrative) {
+		client({
+			method: 'PUT',
+			path: administrative.entity._links.self.href,
+			entity: updatedAdministrative,
+			headers: {
+				'Content-Type': 'application/json',
+				'If-Match': administrative.headers.Etag
+			}
+		}).done(response => {
+			this.loadFromServer(this.state.pageSize);
+		}, response => {
+			if (response.status.code === 412) {
+				alert('DENIED: Unable to update ' +
+					administrative.entity._links.self.href + '. Your copy is stale.');
+			}
+		});
+	}
+	// end::update[]
+
+	// tag::delete[]
+	onDelete(administrative) {
+		client({method: 'DELETE', path: administrative._links.self.href}).done(response => {
+			this.loadFromServer(this.state.pageSize);
+		});
+	}
+	// end::delete[]	
+
+	// tag::navigate[]
+	onNavigate(navUri) {
+		client({method: 'GET', path: navUri}).then(administrativeCollection => {
+			this.links = administrativeCollection.entity._links;
+
+			return administrativeCollection.entity._embedded.administratives.map(administrative =>
+					client({
+						method: 'GET',
+						path: administrative._links.self.href
+					})
+			);
+		}).then(administrativePromises => {
+			return when.all(administrativePromises);
+		}).done(administratives => {
+			this.setState({
+				administratives: administratives,
+				attributes: Object.keys(this.schema.properties),
+				pageSize: this.state.pageSize,
+				links: this.links
+			});
+		});
+	}
+	// end::navigate[]
+
+	// tag::update-page-size[]
+	updatePageSize(pageSize) {
+		if (pageSize !== this.state.pageSize) {
+			this.loadFromServer(pageSize);
+		}
+	}
+	// end::update-page-size[]
+
+	componentDidMount() {
+		this.loadFromServer(this.state.pageSize);
+	}
+
+	render() {
 		return (
-            <div>
-                <AdministrativeList administratives={this.state.administratives}/>
-            </div>
+			<div>
+				<CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+				<AdministrativeList administratives={this.state.administratives}
+							  links={this.state.links}
+							  pageSize={this.state.pageSize}
+							  attributes={this.state.attributes}
+							  onNavigate={this.onNavigate}
+							  onUpdate={this.onUpdate}
+							  onDelete={this.onDelete}
+							  updatePageSize={this.updatePageSize}/>
+			</div>
 		)
 	}
 }
