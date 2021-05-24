2230ca6ec0e5457a0150ed7e39c8ef2f072a9591
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-24 16:15:28 -0300

    Update AdministrativeRepository.java
    Necessary to support paginating

--- src/main/java/com/pablo/repositories/AdministrativeRepository.java
+++ src/main/java/com/pablo/repositories/AdministrativeRepository.java
@@ -1,11 +1,12 @@
 package com.pablo.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pablo.domain.Administrative;
 
 @Repository
-public interface AdministrativeRepository extends JpaRepository<Administrative, Integer> {
+public interface AdministrativeRepository
+		extends PagingAndSortingRepository<Administrative, Integer> {
 
 }
