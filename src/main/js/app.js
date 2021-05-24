d70b5d32f9d7190c60af011be7a7a5843df1045a
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-24 16:11:43 -0300

    remove comments

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,20 +1,3 @@
-// import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-
-
-// export class App extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Welcome to React Front End Served by Spring Boot</h1>
-//             </div>
-//         );
-//     }
-// }
-
-// export default App;
-
-// ReactDOM.render(<App />, document.querySelector("#react3"));
 'use strict';
 
 // tag::vars[]
