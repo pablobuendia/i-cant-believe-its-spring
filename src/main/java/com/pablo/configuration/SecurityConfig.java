77e46fbafb132362f2473232fc0350113025b6a0
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-30 01:19:31 -0300

    Some basic security config

--- src/main/java/com/pablo/configuration/SecurityConfig.java
+++ src/main/java/com/pablo/configuration/SecurityConfig.java
@@ -1,28 +1,35 @@
 package com.pablo.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.pablo.domain.Boss;
+import com.pablo.services.SpringDataJpaUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity security) throws Exception {
-		security.httpBasic().disable();
-		security.cors().and().csrf().disable();
+	@Autowired
+	private SpringDataJpaUserDetailsService userDetailsService;
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userDetailsService)
+				.passwordEncoder(Boss.PASSWORD_ENCODER);
 	}
 
 	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// Web resources
-		web.ignoring().antMatchers("/css/**");
-		web.ignoring().antMatchers("/scripts/**");
-		web.ignoring().antMatchers("/built/**");
-		web.ignoring().antMatchers("/**");
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/built/**", "/css/main.css").permitAll()
+				.anyRequest().authenticated().and().formLogin()
+				.defaultSuccessUrl("/", true).permitAll().and().httpBasic().and().csrf()
+				.disable().logout().logoutSuccessUrl("/");
 	}
 }
