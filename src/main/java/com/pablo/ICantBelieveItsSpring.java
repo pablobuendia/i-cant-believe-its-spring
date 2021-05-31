667d7dda45a01d5f7cf839f6f05b68dccc6d0c10
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-05-31 00:42:14 -0300

    remove funnily obvious comment

--- src/main/java/com/pablo/ICantBelieveItsSpring.java
+++ src/main/java/com/pablo/ICantBelieveItsSpring.java
@@ -3,8 +3,7 @@ package com.pablo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // <-- Look it says Spring Boot, that's what I'm using! Isn't it
-						// nice?
+@SpringBootApplication
 public class ICantBelieveItsSpring {
 
 	public static void main(String[] args) {
