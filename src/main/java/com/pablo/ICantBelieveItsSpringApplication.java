6fe61233343f4c2bd20d91274e712e473cdd6ae9
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-06-20 23:29:51 -0300

    Rename SpringApplication

--- src/main/java/com/pablo/ICantBelieveItsSpringApplication.java
+++ src/main/java/com/pablo/ICantBelieveItsSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ICantBelieveItsSpring {
+public class ICantBelieveItsSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ICantBelieveItsSpring.class, args);
+		SpringApplication.run(ICantBelieveItsSpringApplication.class, args);
 	}
 
 }
