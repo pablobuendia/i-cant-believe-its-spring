6421cb943e8959948306e311e2bcf57b9a2084a0
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-06-21 00:19:56 -0300

    renaming to interface

--- src/main/java/com/pablo/repositories/DoctorRepository.java
+++ src/main/java/com/pablo/repositories/DoctorRepository.java
@@ -1,84 +1,17 @@
 package com.pablo.repositories;
 
-import java.util.Optional;
+import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import com.pablo.domain.Doctor;
 
-/**
- * CrudRepository where T is the domain type that the repository manages. ID is the type
- * of the id of the entity that repository manages.
- * 
- * @author Pablo
- *
- */
-public class DoctorRepository implements CrudRepository<Doctor, String> {
+public interface DoctorRepository extends Repository<Doctor, String> {
 
-	@Override
-	public <S extends Doctor> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	List<Doctor> findAll();
 
-	@Override
-	public <S extends Doctor> Iterable<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	Doctor save(Doctor manager);
 
-	@Override
-	public Optional<Doctor> findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean existsById(String id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterable<Doctor> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterable<Doctor> findAllById(Iterable<String> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void deleteById(String id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void delete(Doctor entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends Doctor> entities) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-
-	}
+	List<Doctor> findByLocationAndSpecialty(String location, String specialty);
 
 }
