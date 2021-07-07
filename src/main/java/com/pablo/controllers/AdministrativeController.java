7f6dacacdd2419ebe851acdf2df66860e03a8b90
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-07-07 19:52:26 -0300

    Rename AdministrativeController

--- src/main/java/com/pablo/controllers/AdministrativeController.java
+++ src/main/java/com/pablo/controllers/AdministrativeController.java
@@ -2,7 +2,6 @@ package com.pablo.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,40 +14,37 @@ import com.pablo.domain.Administrative;
 import com.pablo.services.AdministrativeService;
 
 @RestController
-public class AdministrativeRestController {
+public class AdministrativeController {
 
-	@Autowired
-	AdministrativeService administrativesService;
+	private AdministrativeService administrativesService;
 
-	// creating a get mapping that retrieves all the administratives detail from the
-	// database
-	@GetMapping("/administrative")
-	public List<Administrative> getAllAdministrative() {
-		return administrativesService.getAllAdministrative();
+	public AdministrativeController(AdministrativeService administrativesService) {
+		this.administrativesService = administrativesService;
 	}
 
-	// creating a get mapping that retrieves the detail of a specific administrative
+	@GetMapping("/administrative")
+	public List<Administrative> getAllAdministrative() {
+		return administrativesService.getAll();
+	}
+
 	@GetMapping("/administrative/{administrativeid}")
 	public Administrative getAdministrative(
 			@PathVariable("administrativeid") int administrativeid) {
-		return administrativesService.getAdministrativeById(administrativeid);
+		return administrativesService.getById(administrativeid);
 	}
 
-	// creating a delete mapping that deletes a specified administrative
 	@DeleteMapping("/administrative/{administrativeid}")
 	public void deleteAdministrative(
 			@PathVariable("administrativeid") int administrativeid) {
 		administrativesService.delete(administrativeid);
 	}
 
-	// creating post mapping that post the administrative detail in the database
 	@PostMapping("/administratives")
 	public int saveAdministrative(@RequestBody Administrative administratives) {
 		administrativesService.saveOrUpdate(administratives);
 		return administratives.getId();
 	}
 
-	// creating put mapping that updates the administrative detail
 	@PutMapping("/administratives")
 	public Administrative update(@RequestBody Administrative administratives) {
 		administrativesService.saveOrUpdate(administratives);
