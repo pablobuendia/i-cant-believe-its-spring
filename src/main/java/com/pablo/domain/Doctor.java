acab384854aa520f146319da89ef3f170f68c3a5
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-07-08 23:48:38 -0300

    Refactor Doctor entity to inherit from Person

--- src/main/java/com/pablo/domain/Doctor.java
+++ src/main/java/com/pablo/domain/Doctor.java
@@ -9,38 +9,20 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "doctor")
-public class Doctor {
+public class Doctor extends Person {
 
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String firstName;
-	private String lastName;
+	private Long id;
 	private String specialty;
 
-	public int getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}
 
 	public String getSpecialty() {
