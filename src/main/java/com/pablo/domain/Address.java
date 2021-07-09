a8fbdbd5f987b52ea8bae8b5377e74a45f33ccf5
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-07-09 00:06:41 -0300

    Refactor Address, change id type to Long

--- src/main/java/com/pablo/domain/Address.java
+++ src/main/java/com/pablo/domain/Address.java
@@ -14,7 +14,7 @@ public class Address {
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Long id;
 
 	@Column(nullable = false, length = 50)
 	private String streetName;
@@ -33,6 +33,89 @@ public class Address {
 
 	@Column(nullable = false, length = 50)
 	private String country;
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", streetName=" + streetName + ", streetNumber="
+				+ streetNumber + ", city=" + city + ", district=" + district
+				+ ", province=" + province + ", country=" + country + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((district == null) ? 0 : district.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((province == null) ? 0 : province.hashCode());
+		result = prime * result + ((streetName == null) ? 0 : streetName.hashCode());
+		result = prime * result + ((streetNumber == null) ? 0 : streetNumber.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		}
+		else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		}
+		else if (!country.equals(other.country))
+			return false;
+		if (district == null) {
+			if (other.district != null)
+				return false;
+		}
+		else if (!district.equals(other.district))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		}
+		else if (!id.equals(other.id))
+			return false;
+		if (province == null) {
+			if (other.province != null)
+				return false;
+		}
+		else if (!province.equals(other.province))
+			return false;
+		if (streetName == null) {
+			if (other.streetName != null)
+				return false;
+		}
+		else if (!streetName.equals(other.streetName))
+			return false;
+		if (streetNumber == null) {
+			if (other.streetNumber != null)
+				return false;
+		}
+		else if (!streetNumber.equals(other.streetNumber))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getStreetName() {
 		return streetName;
@@ -80,74 +163,6 @@ public class Address {
 
 	public void setCountry(String country) {
 		this.country = country;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((country == null) ? 0 : country.hashCode());
-		result = prime * result + ((district == null) ? 0 : district.hashCode());
-		result = prime * result + ((province == null) ? 0 : province.hashCode());
-		result = prime * result + ((streetName == null) ? 0 : streetName.hashCode());
-		result = prime * result + ((streetNumber == null) ? 0 : streetNumber.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Address other = (Address) obj;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		}
-		else if (!city.equals(other.city))
-			return false;
-		if (country == null) {
-			if (other.country != null)
-				return false;
-		}
-		else if (!country.equals(other.country))
-			return false;
-		if (district == null) {
-			if (other.district != null)
-				return false;
-		}
-		else if (!district.equals(other.district))
-			return false;
-		if (province == null) {
-			if (other.province != null)
-				return false;
-		}
-		else if (!province.equals(other.province))
-			return false;
-		if (streetName == null) {
-			if (other.streetName != null)
-				return false;
-		}
-		else if (!streetName.equals(other.streetName))
-			return false;
-		if (streetNumber == null) {
-			if (other.streetNumber != null)
-				return false;
-		}
-		else if (!streetNumber.equals(other.streetNumber))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Address [streetName=" + streetName + ", streetNumber=" + streetNumber
-				+ ", city=" + city + ", district=" + district + ", province=" + province
-				+ ", country=" + country + "]";
 	}
 
 }
