dc7857d597e92789da751c9762728e4e71db9aeb
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-07-11 01:17:10 -0300

    Add salary field to Doctor entity

--- src/main/java/com/pablo/domain/Doctor.java
+++ src/main/java/com/pablo/domain/Doctor.java
@@ -1,29 +1,15 @@
 package com.pablo.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "doctor")
 public class Doctor extends Person {
 
-	@Id
-	@Column
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	private String specialty;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+	private Double salary;
 
 	public String getSpecialty() {
 		return specialty;
@@ -31,6 +17,14 @@ public class Doctor extends Person {
 
 	public void setSpecialty(String specialty) {
 		this.specialty = specialty;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
 	}
 
 }
