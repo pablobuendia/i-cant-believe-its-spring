9acd5a193a0e1dbbd93bf35f17ead24457b18afd
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-07-11 22:38:25 -0300

    create doctordata fields

--- src/main/java/com/pablo/dto/DoctorData.java
+++ src/main/java/com/pablo/dto/DoctorData.java
@@ -2,4 +2,44 @@ package com.pablo.dto;
 
 public class DoctorData {
 
+	private String id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String salary;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
 }
