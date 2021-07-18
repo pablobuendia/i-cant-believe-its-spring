8f4682c2947afee0f9f344cb5cf5f50d6ec1c584
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2021-07-17 23:19:25 -0300

    Update MessageTest.java

--- src/test/java/com/pablo/MessageTest.java
+++ src/test/java/com/pablo/MessageTest.java
@@ -1,16 +1,27 @@
 package com.pablo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.util.ObjectUtils;
 
 import com.pablo.helpers.HelloMessage;
 
 public class MessageTest {
 
+	private MessageTest(TestInfo info) {
+		System.out.println("Working on test: " + info.getDisplayName());
+	}
+
+	@DisplayName("<= Message is empty =>")
 	@Test
-	public void messageEmpty() throws Exception {
+	void messageEmpty() throws Exception {
 		HelloMessage message = new HelloMessage();
-		assertTrue(message.getName().isEmpty());
+		assertNull(ObjectUtils.isEmpty(message.getName()),
+				"hellomessage should have no name");
+		assertNull(ObjectUtils.isEmpty(message.getName()),
+				() -> "HelloMessage should have no name");
 	}
 }
